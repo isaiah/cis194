5ca53339db4f3f35599fcb4d1eec395f4cb15a28
Isaiah Peng <issaria@gmail.com>
2015-05-31 00:17:01 +0200

    resolve foldTree

--- src/Ch04.hs
+++ src/Ch04.hs
@@ -24,13 +24,20 @@ fun2' = undefined
 data Tree a = Leaf
             | Node Integer (Tree a) a (Tree a)
               deriving (Show, Eq)
+
+height :: Tree a -> Integer
+height Leaf = 0
+height (Node _ l _ r) = 1 + max (height l) (height r)
+
 foldTree :: [a] -> Tree a
-foldTree l =
-  go l Leaf
+foldTree =
+  foldr insert Leaf
   where
-    go :: [a] -> Tree a -> Tree a
-    go [] tree = tree
-    go (x:xs) tree =
-      case tree of
-        Leaf -> Node 1 Leaf x Leaf
-        Node 
+    insert :: a -> Tree a -> Tree a
+    insert x Leaf = Node 0 Leaf x Leaf
+    insert x (Node _ l v r)
+          | height l <= height r =
+            let left = insert x l in
+                Node (height left) left v r
+          | otherwise =
+                Node (height l) l v (insert x r)
